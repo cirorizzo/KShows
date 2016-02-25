ca4c3e9f931906a2f9d500e0ff00ef92758f8e8b
cirorizzo <ciro.rizzo@gmail.com>
2016-02-25 15:30:24 +0000

    Initial Commit

--- app/src/main/java/com/github/cirorizzo/kshows/model/cats/Cats.kt
+++ app/src/main/java/com/github/cirorizzo/kshows/model/cats/Cats.kt
@@ -8,4 +8,3 @@ data class Cats(var data: Data? = null)
 data class Data(var images: ArrayList<Image>? = null)
 
 data class Image(var url: String? = "", var id: String? = "", var source_url: String? = "")
-
